899ac3051ee641af8605a1a3fedecb1811f2e1ee
marina601 <marina601@live.com>
2021-06-06 20:06:48 +0000

    Added login functionality

--- app.py
+++ app.py
@@ -27,11 +27,6 @@ def index():
     return render_template("index.html", products=products)
 
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -45,7 +40,6 @@ def register():
         if existing_user:
             flash("Username in use, try alternative")
             return redirect(url_for("register"))
-        
 
         # check if the password_1 matches to password_2
         if password_1 != password_2:
@@ -64,6 +58,34 @@ def register():
         flash("You have successfully Registered!")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # check if the user already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # check if the hashed password matches the user input
+            if check_password_hash(existing_user
+                                   ["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}"
+                      .format(request.form.get("username").capitalize()))
+
+            else:
+                # invalid password match
+                flash("You have enetered incorrect Username and/or Password")
+                return redirect(url_for('login'))
+
+        else:
+            # invalid username match
+            flash("You have enetered incorrect Username and/or Password")
+            return redirect(url_for('login'))
+
+    return render_template("login.html")
 
 
 @app.route("/products")
