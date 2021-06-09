3cf0ee599655bac465ac3abdf7b6f64d06b9bdf0
marina601 <marina601@live.com>
2021-06-09 19:55:24 +0000

    Added full product specification and textarea element for review

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
     $('.carousel').carousel();
+    $('#textarea1').val('Write Your Review Here..');
+  M.textareaAutoResize($('#textarea1'));
     
   });
