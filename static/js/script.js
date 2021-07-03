ee6cb023a0ef9bb46ca79992ef692aea1c6ebd4a
marina601 <marina601@live.com>
2021-07-03 20:13:26 +0000

    Removed commented out code

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
     $('.carousel').carousel();
-   // $('#textarea1').val('Write Your Review Here..');
-  //M.textareaAutoResize($('#textarea1'));
   
   // select option for add product page
   $('select').formSelect();
