450d1a705e08c7608f2fa1ae9bd6f859c20d253f
marina601 <marina601@live.com>
2021-07-06 09:03:13 +0000

    Set the debug to False

--- app.py
+++ app.py
@@ -441,4 +441,4 @@ def no_connection(e):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
