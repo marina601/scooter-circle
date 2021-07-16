e60653d55e789ef39421193f7d7d6379ca4a9f71
marina601 <marina601@live.com>
2021-07-16 16:24:10 +0000

    Added comments to the function code

--- static/js/products.js
+++ static/js/products.js
@@ -1,4 +1,15 @@
+//jshint esversion: 6
+/*globals $:false */
+
 $(document).ready(function () {
+    /**
+     * Function which on hover of the icon
+     * Appends a new <span> element to HTML
+     * Custom message for each icon is displayed
+     * To tell the user what the icon represents
+     * Then fade out method is used
+     * to remove the last <span> element fast 
+     */
     $(".speed").hover(
         function() {
             $(this).append($("<span class='hover-effect'>max speed</span>"));
@@ -6,7 +17,6 @@ $(document).ready(function () {
             $(this).find("span").last().fadeOut("fast");
         }
     );
-
     $(".battery").hover(
         function() {
             $(this).append($("<span class='hover-effect'>charging time</span>"));
@@ -14,7 +24,6 @@ $(document).ready(function () {
            $(this).find("span").last().fadeOut("fast");
         }
     );
-
     $(".range").hover(
         function() {
             $(this).append($("<span class='hover-effect'>max range</span>"));
